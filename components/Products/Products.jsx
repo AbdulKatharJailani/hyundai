284237a6bf666d271c62240ed1fcfb218d3463ba
AbdulRassic <abdul.sulaiman@jaam.co>
2022-11-22 16:39:08 +0530

    products card section

--- components/Products/Products.jsx
+++ components/Products/Products.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import { images } from '../../constants';
+
+const cardDetails = [
+  { name: "Generator", cardImage: images.homepage_block },
+  { name: "Gardening tools", cardImage: images.homepage_block2 },
+  { name: "Washers and Compressors", cardImage: images.homepage_block3 },
+  { name: "Renovation and Construction", cardImage: images.homepage_block4 },
+  { name: "DIY", cardImage: images.homepage_block5 },
+  { name: "Cordless tools", cardImage: images.homepage_block4 }
+]
+
+
 
 const Products = () => {
   return (
-    <div>
+    <div className='products__container'>
       <h1 className='products__header'>Discover the world of Hyundai</h1>
+      <div className='products__card-section'>
+        {cardDetails.map((cardDetail) =>
+          <div>
+            <Image src={cardDetail.cardImage} className="products__image" />
+            <div className="products__name">
+              <h2>{cardDetail.name}</h2>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
